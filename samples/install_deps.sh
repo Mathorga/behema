177e9807b578fc44e3c6284e43479d102c52c685
Luka Micheletti <michelettiluka@gmail.com>
2021-12-10 21:18:00 +0100

    Added installation script for samples

---
+++ samples/install_deps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install C++ compiler.
+sudo apt install g++
+
+# Install SFML graphics library.
+sudo apt install libsfml-dev
+
+# Install OpenCV for camera access.
+#sudo apt install libopencv-dev
+sudo apt install libopencv-core-dev
+sudo apt install libopencv-videoio-dev
