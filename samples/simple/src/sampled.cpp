ce19d76e3dd1232b31cf9745e8c655b466ebc861
Luka Micheletti <michelettiluka@gmail.com>
2021-12-22 11:07:30 +0100

    Updated sampled

--- samples/simple/src/sampled.cpp
+++ samples/simple/src/sampled.cpp
@@ -5,11 +5,10 @@
 #include <portia/portia.h>
 
 void print(field2d_t* field) {
-    system("clear");
     for (field_size_t y = 0; y < field->height; y++) {
         for (field_size_t x = 0; x < field->width; x++) {
             neuron_t currentNeuron = field->neurons[IDX2D(x, y, field->width)];
-            printf("%c ", currentNeuron.value >= field->fire_threshold ? '@' : ' ');
+            printf("%c ", currentNeuron.value >= field->fire_threshold ? '@' : '.');
         }
         printf("\n");
     }
@@ -20,6 +19,8 @@ int main(int argc, char **argv) {
     field_size_t field_width = 100;
     field_size_t field_height = 60;
     nh_radius_t nh_radius = 2;
+    ticks_count_t sampleWindow = 10;
+    ticks_count_t samplingBound = sampleWindow - 1;
 
     // Input handling.
     switch (argc) {
@@ -50,7 +51,7 @@ int main(int argc, char **argv) {
     f2d_set_pulse_window(&even_field, 0x3A);
     f2d_set_syngen_beat(&even_field, 0.1F);
     f2d_set_max_touch(&even_field, 0.2F);
-    f2d_set_sample_window(&even_field, 100);
+    f2d_set_sample_window(&even_field, sampleWindow);
     odd_field = *f2d_copy(&even_field);
 
     field_size_t lInputsCoords[] = {10, 5, 40, 20};
@@ -58,7 +59,7 @@ int main(int argc, char **argv) {
 
     ticks_count_t* lInputs = (ticks_count_t*) malloc((lInputsCoords[2] - lInputsCoords[0]) * (lInputsCoords[3] - lInputsCoords[1]) * sizeof(ticks_count_t));
     ticks_count_t* rInputs = (ticks_count_t*) malloc((rInputsCoords[2] - rInputsCoords[0]) * (rInputsCoords[3] - rInputsCoords[1]) * sizeof(ticks_count_t));
-    ticks_count_t sample_step = even_field.sample_window;
+    ticks_count_t sample_step = samplingBound;
 
     srand(time(NULL));
 
@@ -66,44 +67,39 @@ int main(int argc, char **argv) {
         field2d_t* prev_field = i % 2 ? &odd_field : &even_field;
         field2d_t* next_field = i % 2 ? &even_field : &odd_field;
 
+        system("clear");
+
         // Only get new inputs according to the sample rate.
-        if (sample_step == prev_field->sample_window) {
+        if (sample_step > samplingBound) {
             // Fetch input.
             for (field_size_t y = lInputsCoords[1]; y < lInputsCoords[3]; y++) {
                 for (field_size_t x = lInputsCoords[0]; x < lInputsCoords[2]; x++) {
-                    lInputs[IDX2D(x - lInputsCoords[0], y - lInputsCoords[1], lInputsCoords[2] - lInputsCoords[0])] = (rand() % (prev_field->sample_window - 1));
+                    lInputs[IDX2D(x - lInputsCoords[0], y - lInputsCoords[1], lInputsCoords[2] - lInputsCoords[0])] = (rand() % (samplingBound));
                 }
             }
 
             for (field_size_t y = rInputsCoords[1]; y < rInputsCoords[3]; y++) {
                 for (field_size_t x = rInputsCoords[0]; x < rInputsCoords[2]; x++) {
-                    rInputs[IDX2D(x - rInputsCoords[0], y - rInputsCoords[1], rInputsCoords[2] - rInputsCoords[0])] = (rand() % (prev_field->sample_window - 1));
+                    rInputs[IDX2D(x - rInputsCoords[0], y - rInputsCoords[1], rInputsCoords[2] - rInputsCoords[0])] = (rand() % (samplingBound));
                 }
             }
+
             sample_step = 0;
         }
+
+        printf("%d\n", sample_step);
 
         // Feed the field.
         f2d_sample_sqfeed(prev_field, lInputsCoords[0], lInputsCoords[1], lInputsCoords[2], lInputsCoords[3], sample_step, lInputs, DEFAULT_CHARGE_VALUE);
         f2d_sample_sqfeed(prev_field, rInputsCoords[0], rInputsCoords[1], rInputsCoords[2], rInputsCoords[3], sample_step, rInputs, DEFAULT_CHARGE_VALUE);
 
+        sample_step++;
+
         print(next_field);
-        // system("clear");
-        printf("%d\n", sample_step);
-        // for (field_size_t y = lInputsCoords[1]; y < lInputsCoords[3]; y++) {
-        //     for (field_size_t x = lInputsCoords[0]; x < lInputsCoords[2]; x++) {
-        //         ticks_count_t currentInput = lInputs[IDX2D(x - lInputsCoords[0], y - lInputsCoords[1], lInputsCoords[2] - lInputsCoords[0])];
-        //         printf("%d\t", currentInput);
-        //     }
-        //     printf("\n");
-        // }
-        // printf("\n");
 
         // Tick the field.
         f2d_tick(prev_field, next_field);
 
-        sample_step++;
-
-        usleep(100000);
+        usleep(1000000);
     }
 }
