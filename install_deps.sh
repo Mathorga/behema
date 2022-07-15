71dd5a4471140c8f049331a825994130fc71198a
Luka Micheletti <32356959+Mathorga@users.noreply.github.com>
2022-07-15 17:10:14 +0200

    Update install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 # Install Make.
-sudo apt install make
+sudo apt install make -y
 
 # Install C compiler.
-sudo apt install gcc
+sudo apt install gcc -y
 
 # Install simple image manipulation library. Handles pbm, pgm and ppm image file formats.
 #sudo apt install netpbm
+
+# Install OpenCL.
+sudo apt install opencl-headers ocl-icd-opencl-dev -y
 
 printf "\nDependencies installed!\n"
