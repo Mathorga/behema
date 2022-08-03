c754ddd521d029f71caf94e24fd113e5b7d7007b
Luka Micheletti <michelettiluka@gmail.com>
2022-08-03 23:52:53 +0200

    Added OpenCL sources

---
+++ src/behema_cl.c
@@ -0,0 +1,1 @@
+#include "behema_cl.h"
---
+++ src/behema_cl.h
@@ -0,0 +1,16 @@
+/*
+*****************************************************************
+behema_cuda.h
+
+Copyright (C) 2022 Luka Micheletti
+*****************************************************************
+*/
+
+#ifndef __BEHEMA_CL__
+#define __BEHEMA_CL__
+
+#include "cortex.h"
+#include "error.h"
+#include "utils.h"
+
+#endif
