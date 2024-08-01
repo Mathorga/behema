5ce67f4a8dd88e65325f6bc7010b3169e10aca13
Luka Micheletti <michelettiluka@gmail.com>
2024-08-01 11:49:30 +0200

    Added proper error checking on init

--- src/error.h
+++ src/error.h
@@ -7,7 +7,8 @@ typedef enum {
     ERROR_FILE_DOES_NOT_EXIST = 2,
     ERROR_FILE_SIZE_WRONG = 3,
     ERROR_FAILED_ALLOC = 4,
-    ERROR_CORTEX_UNALLOC = 5
+    ERROR_CORTEX_UNALLOC = 5,
+    ERROR_SIZE_WRONG = 6
 } error_code_t;
 
 #endif
